b776d69b0e74d3bf7901993ead230480cfc521b2
atheeqsyed <atheeqmvj@gmail.com>
2022-07-17 20:11:36 +0530

    learning php array

--- phparray.php
+++ phparray.php
@@ -1,16 +1,40 @@
 
 <?php
- 
-$intArray = array( 10, 20 , 30);
- 
+
+$intArray = array(10, 20, 30);
+
 echo "First Element: $intArray[0]\n";
 echo "<br>";
 echo "Second Element: $intArray[1]\n";
 echo "<br>";
 echo "Third Element: $intArray[2]\n\n";
 echo "<br>";
- 
+
 //returns data type and value
 var_dump($intArray);
- 
+
+$name = 'ATEEQ';
+//echo "result from php funtion =>" . strrev($name) . "<BR>";
+//$j = 0;
+//for ($i = 0; $i <= 10; $i++) {
+//  if ($name[$i] == '') {
+//    break;
+//}
+// echo "$name[$i]";
+//$j++;
+//}
+//echo "$j";
+//$reverse = '';
+//for ($z = $j; $z >= 0; $z--) {
+//  $reverse = $reverse . "$name[$z]";
+//}
+//echo $reverse;
+$reverse = '';
+for ($i = 0; $i < 5; $i++) {
+    $reverse = $name[$i] . $reverse;
+}
+echo $reverse;
+
+
+
 ?>
