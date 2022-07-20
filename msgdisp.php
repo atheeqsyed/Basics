79c0cd9e882625cba2a2d97b9b4d0f98d5fec0a5
atheeqsyed <atheeqmvj@gmail.com>
2022-07-20 18:40:37 +0530

    Alert popup added

---
+++ msgdisp.php
@@ -0,0 +1,13 @@
+<form name="testForm" id="testForm" method="POST">
+    <input type="submit" name="btnpost" value="submit" autofocus onclick="return true;" />
+</form>
+<?php
+$alertmessage = "Form submitted!";
+if (isset($_POST["btnpost"])) {
+    echo "
+<script type = \"text/javascript\">
+alert('$alertmessage');
+</script>
+";
+}
+?>
